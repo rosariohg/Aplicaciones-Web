26beb0f1252814f3211b73aa3ccfd434f68d378b
rosario <rhuancagonza@gmail.com>
2017-04-12 10:45:18 -0500

    .

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var http = require('http'),
 	fs = require('fs');
 
-var html = fs.readFileSync('./index.html');
-
 http.createServer(function(solicitud,respuesta){
-	respuesta.write(html);
-	respuesta.end();
+	fs.readFile('./index.html',function(error,html){
+		respuesta.write(html);
+		respuesta.end();		
+	});
+	
 }).listen(8080);
