8a49932a8197a9cffad8a5ef9785604d392cefc9
rosario <rhuancagonza@gmail.com>
2017-05-17 22:59:35 -0500

    lab 9

--- lab8/login_fabvision/index.js
+++ lab8/login_fabvision/index.js
@@ -1,18 +1,24 @@
 var express = require('express')
 var app = express()
-var path = require('path')
+
+// var path = require('path')
 
 app.use(express.static('public'))
+app.set('view engine','jade');
 
-app.get('/', function(req,res){
-	res.sendFile(path.join(__dirname+'/views/login.html'));
+// app.get('/', function(req,res){
+// 	res.sendFile(path.join(__dirname+'/views/login.html'));
+// })
+
+app.get('/prueba-jade',function(req,res){
+	res.render('index')
 })
 
-app.use(function (err,req,res,next){
-	console.log(err.stack)
-	res.status(500).send('Algo salio mal')
-})
+// app.use(function (err,req,res,next){
+// 	console.log(err.stack)
+// 	res.status(500).send('Algo salio mal')
+// })
 
 app.listen(3000,function(){
-	console.log('Escuchando en el puerto 3000')
+	console.log('Escuchando en el puerto 3000°');
 });
